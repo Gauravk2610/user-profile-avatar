7fab341eedb255333ab2e49d4e223b1513df952e
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2021-12-09 02:05:15 +0530

    Test file Updated

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import Avatar from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Avatar src='https://www.tedxace.com/img/team/2020/gauravkonde.jpeg' alt='profile' size={80} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
