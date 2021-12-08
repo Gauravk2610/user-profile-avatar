a08c273fd2b008496e61a449ac02ae243fc47b6e
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2021-12-09 02:02:02 +0530

    Prettier Formatted Document

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 
 interface avatarInterface {
-  src: string,
-  alt: string,
-  size: number
+  src: string;
+  alt: string;
+  size: number;
 }
 
 const Avatar = ({ src, alt, size }: avatarInterface) => {
-  return <img src={src} alt={alt} width={size} height={size} style={{borderRadius: 100, objectFit: 'cover'}} />;
+  return (
+    <img
+      src={src}
+      alt={alt}
+      width={size}
+      height={size}
+      style={{ borderRadius: 100, objectFit: 'cover' }}
+    />
+  );
 };
 
-export default Avatar
+export default Avatar;
