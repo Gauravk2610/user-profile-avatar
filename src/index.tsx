8c5cf3eaf27bfed717c45ab71f4ac37e9b7cbc6e
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2021-12-09 02:00:27 +0530

    added Interfaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 
+interface avatarInterface {
+  src: string,
+  alt: string,
+  size: number
+}
 
-export const Avatar: React.FC<{ src: string, alt: string, size: number }>= ({ src, alt, size }) => {
+export const Avatar = ({ src, alt, size }: avatarInterface) => {
   return <img src={src} alt={alt} width={size} height={size} style={{borderRadius: 100, objectFit: 'cover'}} />;
 };
